d3c8df747a4b2dde58b56a01af1cc75aeff13414
zautomata <zautomata@gmail.com>
2018-05-16 15:21:30 +0200

    update

--- main.sh
+++ main.sh
@@ -1,10 +1,12 @@
 #!/bin/sh
 
-. $1 # load the configuration file
+config=$1 # load the configuration file
+. $config # load the configuration file
+
 echo $username
 start(){
 	echo "starting $1.sh"
-	ftp -V "https://raw.githubusercontent.com/zautomata/openbsd.sh/master/$1.sh" >$1.sh;chmod +x $1.sh;$1.sh;
+	ftp -V "https://raw.githubusercontent.com/zautomata/openbsd.sh/master/$1.sh" >$1.sh;chmod +x $1.sh;$1.sh $config;
 }
 
 #start mail
--- networking.sh
+++ networking.sh
@@ -1,4 +1,5 @@
 #!/bin/sh
+. $1 # load given configuration file
 
 echo "networking shell script in action"
 echo $username
